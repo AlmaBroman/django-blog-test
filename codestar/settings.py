51769ca1182e245fe0ec6199526b414f26819e20
Alma Broman <alma.broman@gmail.com>
2023-11-26 21:38:33 +0000

    Deployment commit 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['8000-almabroman-djangoblogte-y7x0n6tyofo.ws-eu106.gitpod.io', 'django-blog-test-a-40461ba718ba.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-almabroman-djangoblogte-y7x0n6tyofo.ws-eu106.gitpod.io', 'https://django-blog-test-a-40461ba718ba.herokuapp.com']
